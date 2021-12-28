ea153d1fa6808bcca81ff5e7f806b42611a0e910
SyrupOnWaffles <nicholaskor@hotmail.com>
2021-12-28 16:19:36 -0700

    Improve key selection, add tecsee carrots, add switch selection system (to be improved in future), prolly some other stuff

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ import data
 from pynput import keyboard
 import random
 def on_press(key):
-    print(str(format(key)).replace("Key.", ''))
-    try:
-        # keys found in the list
-        pygame.mixer.Sound.play(data.keylist[str(format(key)).replace("Key.", '').upper()])
-    except:
-        # keys not found in the list
-        pygame.mixer.Sound.play(random.choice(list(data.keylist.values())))
-def main():
+    try:     
+        pygame.mixer.Sound.play(data.keylist[format(key.char).upper()])
+    except AttributeError:
+        pygame.mixer.Sound.play(data.keylist[str(format(key)).upper().replace("KEY.", '')])
+def startUp():
     pygame.init()
     print(data.asciiArt)
+    print("                     Enjoy the " + data.switch)    
+def main():
     run = True
     while run:
         with keyboard.Listener(on_press=on_press) as listener:listener.join()
     print("goodbye!!!")
     pygame.quit()
 if __name__ == '__main__':
+    startUp()
     main()
